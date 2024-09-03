4314675b52b63c7f4dac63b7999bc52fd86bdde7
mt <mt@gmail.com>
2024-09-03 13:57:47 +0800

    优化

--- help.go
+++ help.go
@@ -2,43 +2,22 @@ package kline
 
 // 时间取模算法
 func GetTimeModeling(period string, t int64) int64 {
-	var timestamp int64
-	switch period {
-	case AMinute:
-		timestamp = t - (t % int64(60))
-		break
-	case FiveMinutes:
-		timestamp = t - (t % int64(5*60))
-		break
-	case FifteenMinutes:
-		timestamp = t - (t % int64(15*60))
-		break
-	case Minutes:
-		timestamp = t - (t % int64(30*60))
-		break
-	case AnHour:
-		timestamp = t - (t % int64(60*60))
-		break
-	case TwoHours:
-		timestamp = t - (t % int64(2*60*60))
-		break
-	case FourHours:
-		timestamp = t - (t % int64(4*60*60))
-		break
-	case ADay:
-		timestamp = t - (t % int64(24*60*60))
-		break
-	case AWeek:
-		timestamp = t - (t % int64(7*24*60*60))
-		break
-	case OneMonth:
-		timestamp = t - (t % int64(30*24*60*60))
-		break
-	case AYear:
-		timestamp = t - (t % int64(365*24*60*60))
-		break
-	default:
-		return t
+	// 定义时间段映射
+	periodMap := map[string]int64{
+		AMinute:        60,
+		FiveMinutes:    5 * 60,
+		FifteenMinutes: 15 * 60,
+		Minutes:        30 * 60,
+		AnHour:         60 * 60,
+		TwoHours:       2 * 60 * 60,
+		FourHours:      4 * 60 * 60,
+		ADay:           24 * 60 * 60,
+		AWeek:          7 * 24 * 60 * 60,
+		OneMonth:       30 * 24 * 60 * 60,
+		AYear:          365 * 24 * 60 * 60,
 	}
-	return timestamp
+	if periodValue, exists := periodMap[period]; exists {
+		return t - (t % periodValue)
+	}
+	return t
 }
